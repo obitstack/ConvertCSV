cb19875f22ae02833f17e04000e8d61e121b7899
00bs Station <return0000000@gmail.com>
2021-01-19 21:45:15 +0100

    Added Frame

---
+++ src/UserInterFace.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class UserInterFace extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    public UserInterFace() {
+        this.setSize(300,300);
+        this.setVisible(true);
+        
+
+    }
+    
+}
