1e237d9f833c6881e4bca59569a1f24eb0f4200b
00bs Station <return0000000@gmail.com>
2021-01-19 22:22:16 +0100

    Create simple UI

--- src/UserInterFace.java
+++ src/UserInterFace.java
@@ -4,12 +4,26 @@ import javax.swing.*;
 public class UserInterFace extends JFrame {
 
     private static final long serialVersionUID = 1L;
+    private JPanel panel; 
 
     public UserInterFace() {
-        this.setSize(300,300);
-        this.setVisible(true);
+
+        // panel = new JPanel();
+        this.setTitle("ConvertCSV");
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        Integer scale = dim.height/10;
         
 
+        this.setSize(scale*2,scale*3);
+        this.setResizable(false);
+        
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        // JPanel.setBackground();
+        
+        this.setVisible(true);
     }
+
+  
     
 }
