6c56b0c9f615d7b492ec79fa7621331437a2926b
00bs Station <return0000000@gmail.com>
2021-01-22 18:25:49 +0100

    Added file paths

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
 
 public class Main {
+
+
+    private String fileName = "n26-csv-transactions(1).csv"; 
+    private String filePath = "src/input/";
+    private String outputPath = "src/output";
     public static void main(String[] args) throws Exception {
+
+
+         
+
         
-        new UserInterFace();
+        // new UserInterFace();
+        new FileConverter();
 
 
 
